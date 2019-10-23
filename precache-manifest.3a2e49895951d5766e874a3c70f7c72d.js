efa4d066df9e7afd8571579123b5bdcf9eb8cd70
AntonTretjak <tretjak.anton.dev@gmail.com>
2019-10-23 06:01:00 +0300

    Updates

---
+++ precache-manifest.3a2e49895951d5766e874a3c70f7c72d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "17ca6610df9952e4a0ff7247aee83927",
+    "url": "/anton-tretjak-22-10-2019/index.html"
+  },
+  {
+    "revision": "a80f144f35c9a6869f88",
+    "url": "/anton-tretjak-22-10-2019/static/css/main.28d84280.chunk.css"
+  },
+  {
+    "revision": "8489d613c4aac5ee8702",
+    "url": "/anton-tretjak-22-10-2019/static/js/2.5f6ac84b.chunk.js"
+  },
+  {
+    "revision": "a80f144f35c9a6869f88",
+    "url": "/anton-tretjak-22-10-2019/static/js/main.1a6bb3d4.chunk.js"
+  },
+  {
+    "revision": "3a0fd1d9021a23edfbac",
+    "url": "/anton-tretjak-22-10-2019/static/js/runtime-main.fb4fb576.js"
+  }
+]);
