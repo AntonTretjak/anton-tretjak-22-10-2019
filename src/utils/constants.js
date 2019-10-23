b73f1be83ef579b5d4883fde5b3cbc67438b7b27
AntonTretjak <tretjak.anton.dev@gmail.com>
2019-10-23 06:00:39 +0300

    finally

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,3 @@
 // export const apikey = 'upmElnSDGWSVtzZfcSztAexrOW83wiYB';
-export const apikey = 'iHrKVp9LLKfwdQsjeOPRJHTZqTX33TGV';
+// export const apikey = 'iHrKVp9LLKfwdQsjeOPRJHTZqTX33TGV';
+export const apikey = '1ON1wZSnkAvb6KTCKqPLRuSpc9Izsmzg';
